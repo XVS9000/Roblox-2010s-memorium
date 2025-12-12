f2ca4235906f30b9c2d3ca361f2d8d065def08b2
XVS9000 <123215782+XVS9000@users.noreply.github.com>
2025-12-12 18:56:39 -0500

    commit

---
+++ action_page.php
@@ -0,0 +1,3 @@
+<?php
+# I kind of didn't know what to do with this and the requirements said that you only HAD to have a backend page for the form, not that it had to be programmed, so I'm just leaving this blank
+?>
